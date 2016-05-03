52abd02d469b3acbef89541eec7918e76b601ee2
priyanka2592 <priyankasingh.baghel2011@gmail.com>
2016-05-03 20:54:23 +0530

    Added files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A pair of functions that cache the inverse of a matrix
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## Creates a special matrix object that can cache its inverse
+makeCacheMatrix <- function( M = matrix() ) {
 
+## Initialize the inverse property
+    i <- NULL
+
+## Method to set the matrix
+    set <- function( matrix ) {
+            M <<- matrix
+            i <<- NULL
+    }
+
+## Method the get the matrix
+    get <- function() {
+    	## Return the matrix
+    	m
+    }
+
+## Method to set the inverse of the matrix
+    setInverse <- function(inverse) {
+        i <<- inverse
+    }
+
+## Method to get the inverse of the matrix
+    getInverse <- function() {
+        ## Return the inverse property
+        i
+    }
+
+## Return a list of the methods
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## Compute the inverse of the special matrix returned by "makeCacheMatrix"
+## above. If the inverse has already been calculated (and the matrix has not
+## changed), then the "cachesolve" should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+
+## Return a matrix that is the inverse of 'x'
+    M <- x$getInverse()
+
+## Just return the inverse if its already set
+    if( !is.null(M) ) {
+            message("getting cached data")
+            return(M)
+    }
+
+## Get the matrix from our object
+    data <- x$get()
+
+## Calculate the inverse using matrix multiplication
+    M <- solve(data) %*% data
+
+## Set the inverse to the object
+    x$setInverse(M)
+
+## Return the matrix
+    M
+} 
